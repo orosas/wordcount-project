1f5f2341d85e9a6038a38d55899f7769d584bdcf
Omar Rosas <jorosas@gmail.com>
2020-03-28 21:00:28 -0600

    First Commit Word Count App. Video Django 2.2

---
+++ wordcount/views.py
@@ -0,0 +1,30 @@
+
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+
+
+def homepage(request):
+    return render(request, 'home.html', )
+
+def count(request):
+    fulltext = request.GET['fulltext']
+    
+    word_list = fulltext.casefold().split()
+
+    word_dictionary = {}
+    
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] += 1
+        else:
+            word_dictionary[word] = 1
+    
+    sorted_words = sorted(word_dictionary.items(), key=operator.itemgetter(1), reverse=True)
+
+
+    return render(request, 'count.html', {'fulltext': fulltext, 'count': len(word_list), 'sorted_words': sorted_words})
+
+
+def about(request):
+    return render(request, 'about.html', )
